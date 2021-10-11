5237f8d8e5113c3071f151aaa92f6d9fec674d0a
yahighness <franceshameeka@gmail.com>
2021-10-11 11:54:20 -0400

    Upload first edit of tip calculator

---
+++ tip_calculator.py
@@ -0,0 +1,7 @@
+# this is my tip calculater for success after prison dinner party  
+bill=float(input("what is the total bill amount?: "))
+even_bill_split = int(input( 'how many people are splitting the bill'))
+tip = int (input("enter tip in percentage:"))
+tax = tip_amount =
+))
+tax = .1
