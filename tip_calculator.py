14345bbd2bc06ba469b2df6adf0cc8a8c05b6758
yahighness <90288035+yahighness@users.noreply.github.com>
2021-11-02 04:08:13 -0400

    revised tip calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,18 +1,21 @@
-print('This is my tip calculater for the success after prison dinner party!')
-# this is the command for the total price of our bill 
-bill = float(input("what is the total bill amount?: "))
-bill_f = float(bill)
-# here is the option to apply a tip
-tip = float(input('How much do want to leave your server as a tip?: '))
-tip_f = float(tip)
-# tax applied to bill 
-tax = 0.1
-# total amount of people splitting the bill
+# Welcome Guests : Here is  "Success After Prison Dinner Party"  Tip Calculator. 
+
+# PLEASE INPUT THE TOTAL AMOUNT OF THE BILL
+bill = float(input("What is the total bill amount?: "))
+
+# Please add your gratuity amount as tip for your server
+tip = int(input("What % tip would you like to give?: "))
+tip = float(tip)
+
+# Please input the number of people who will equally split the bill 
 number_of_people = int(input('How many people are splitting the bill?: '))
 number_people_float = float(number_of_people)
-# the amount of total bill
-bill_amt_with_tip = bill_f * (tip_f/100)
-# total after taxes
-total = bill + (bill_amt_with_tip * tax)
+
+# Tax applied to bill 
+tax = 0.1
+
+# Total amount of bill 
+total = bill + (bill * tax)
 split_bill = total/number_of_people
-print(f'Thanks for celebrating with us everbody in your party should pay ${split_bill} and your grand total is ${total}')
+
+print(f'Thanks for celebrating with us everyone should pay ${split_bill} and your grand total is ${total}')
